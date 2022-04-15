2a63aa1bd94f1d7f671b26bd19821c5cd638532f
S1mplyD <lucabennati2k16@gmail.com>
2022-04-15 17:35:18 +0200

    added global variables, low and high prio queue

--- src/main.c
+++ src/main.c
@@ -2,74 +2,81 @@
 #include "asl.h"
 #include "umps3/umps/const.h"
 #include "scheduler.h"
+#include "klog.h"
+#include "exception.h"
 
+int processCount;
+int softBlockCounter;
+struct list_head HI_readyQueue;    //Coda processi ready high priority
+struct list_head LO_readyQueue;    //Coda processi ready low priority
+pcb_t *currentProcess;   //Puntatore al pcb corrente allo stato di running
+int semDevice[49];       //Semaforo dei device
+passupvector_t *passUpVector = (passupvector_t *) PASSUPVECTOR;
+int pid;
+
+extern void uTLB_RefillHandler();
 extern void test();
 
-extern void uTLB_refillHandler();
-
-extern void exceptionHandler();
-
-passupvector_t *passUpVector;
-
-//Da usare in caso non vada 
-state_t initState;
-
 int main(){
-    
-    processCount = 0;           //Contatore processi vivi
-    int softBlockCounter = 0;       //Contatore processi bloccati
-    currentProcess = NULL;   //Puntatore al pcb corrente allo stato di running
+    klog_print("inizio main//");
+    //Inizializzazione passUpVector
+    passUpVector->tlb_refill_handler = (memaddr) uTLB_RefillHandler;
+    passUpVector->tlb_refill_stackPtr = KERNELSTACK;
+    passUpVector->exception_handler = (memaddr) exceptionHandler;
+    passUpVector->exception_stackPtr = KERNELSTACK;
 
-    mkEmptyProcQ(&readyQueue);      //Inizializzo la readyQueue come una lista di PCB vuota
+    klog_print("pass up vector init//");
 
     //Inizializzazione PCB e ASL
     initPcbs();
     initASL();
+    klog_print("pcb asl init//");
 
-    //Inizializzazione passUpVector
-    passUpVector = (passupvector_t*) PASSUPVECTOR;
-    passUpVector->exception_handler = (memaddr) exceptionHandler;
-    passUpVector->exception_stackPtr = (memaddr) KERNELSTACK;
-    passUpVector->tlb_refill_handler = (memaddr) uTLB_refillHandler;
-    passUpVector->tlb_refill_stackPtr = (memaddr) KERNELSTACK;
+    processCount = 0;            //Contatore processi vivi   
+    softBlockCounter = 0;   //Contatore processi bloccati
+    mkEmptyProcQ(&HI_readyQueue);      //Inizializzo la HI_readyQueue come una lista di PCB vuota
+    mkEmptyProcQ(&LO_readyQueue);      //Inizializzo la LO_readyQueue come una lista di PCB vuota
+    currentProcess = NULL;   //Puntatore al pcb corrente allo stato di running
+    pid = 0;
+    klog_print("var init//");
 
     //Inizializzazione device semafori a 0
     int i;
-    for(i = 0; i < DEVICECNT; i++){
+    for(i = 0; i < 49; i++){
         semDevice[i] = 0;
     }
+    klog_print("sem init//");
 
     //Imposto a 100ms l'Interval Timer
     LDIT(PSECOND);
+    klog_print("it set//");
 
     //Creazione processo
     pcb_t *p = allocPcb();
     p->p_prio = PROCESS_PRIO_LOW;               //Setto priorità bassa al processo
-    p->p_s.status = IEPON | IMON | TEBITON;     //Abilitazione degli interrupt
-    p->p_s.cause = LOCALTIMERINT;               //Abilitazione interrupt timer locale
-    p->p_s.gpr[26] = RAMSTART;                  //Setto sp all'inizio della RAM
-    p->p_s.pc_epc = (memaddr) test;             //Imposto PC
-    p->p_s.gpr[24] = &p->p_s.pc_epc;    
+    p->p_s.status = IEPON | IMON | TEBITON | USERPON;     //Abilitazione degli interrupt
     
-
+    RAMTOP(p->p_s.reg_sp);                  //Setto sp all'inizio della RAM
+    
+    p->p_s.pc_epc = (memaddr) test;             //Imposto PC
+    p->p_s.pc_epc = (memaddr) test;
+    p->p_pid = pid;
     //Process Tree Fields to NULL
     p->p_parent = NULL;
     p->p_child.next = NULL;
     p->p_sib.next = NULL;
-    
     //Imposto il tempo del processo a 0
     p->p_time = 0;
-
     //Setto il puntatore al semafor a NULL
     p->p_semAdd = NULL;
-
     //Setto la struttura di supporto a NULL
     p->p_supportStruct = NULL;
-
-    processCount ++;
-
-    currentProcess = NULL;
-    insertProcQ(&(readyQueue),p);
-
+    klog_print("process init//");
+    insertProcQ(&LO_readyQueue,p);
+    klog_print("after insertProcQ//");
+    processCount++;
+    
     scheduler();
+
+    return 0;
 }
