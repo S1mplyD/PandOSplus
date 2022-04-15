9889bf8424ad261354d02d2dd2ea0fab78b099ab
S1mplyD <lucabennati2k16@gmail.com>
2022-04-15 17:33:39 +0200

    added scheduler

--- include/scheduler.h
+++ include/scheduler.h
@@ -5,13 +5,10 @@
 #include "umps3/umps/const.h"
 #include "umps3/umps/libumps.h"
 #include "pcb.h"
+#include "klog.h"
+#include "asl.h"
 
-extern int processCount; //Contatore processi vivi
-   
-extern int softBlockCounter;       //Contatore processi bloccati
-extern pcb_t *readyQueue;    //Coda processi ready
-extern pcb_t *currentProcess;   //Puntatore al pcb corrente allo stato di running
-extern int semDevice[DEVICECNT];       //Semaforo dei device
+
 
 void scheduler();
 
--- src/scheduler.c
+++ src/scheduler.c
@@ -1,32 +1,60 @@
 #include "scheduler.h"
 
-void scheduler(){
-    
-    //Prendo il processo dalla readyQueue
-    currentProcess = removeProcQ(&readyQueue);
+extern struct list_head HI_readyQueue;
+extern struct list_head LO_readyQueue;
+extern int processCount;      // Contatore processi vivi
+extern int softBlockCounter;  // Contatore processi bloccati
+extern pcb_t *currentProcess; // Puntatore al pcb corrente allo stato di running
 
-    //Se la coda dei processi non è vuota
-    if(currentProcess != NULL){
-        //Carico lo stato del processo
-        LDST(&(currentProcess->p_s));
-    }
-    else{
-        if(processCount == 0){
-            HALT();
-        } 
-        else if (processCount > 0 && softBlockCounter > 0)
-        {   
-            //Abilito gli interrupt
-            setSTATUS(IECON | IMON);
-            //Carico un valore altissimo nel PLT
-            setTIMER(9999999999);
+unsigned int process_job_start_time;
 
-            WAIT();
+void scheduler()
+{
+    klog_print("scheduler//");
+    if (emptyProcQ(&HI_readyQueue))
+    {
+        klog_print("HI empty//");
+        if (emptyProcQ(&LO_readyQueue))
+        {  
+            klog_print("LO empty//");
+            if (processCount == 0)
+            {
+                klog_print("No more process");
+                HALT();
+            }
+            else if (processCount > 0 && softBlockCounter > 0)
+            {
+                // Abilito gli interrupt
+                setSTATUS(getSTATUS() | IECON | IMON);
+                // Carico un valore altissimo nel PLT
+                setTIMER(0xFFFFFF);
+
+                WAIT();
+            }
+            else if (processCount > 0 && softBlockCounter == 0)
+            {
+                PANIC();
+            }
         }
-        else if (processCount > 0 && softBlockCounter ==0)
+        else
         {
-            PANIC();
+            klog_print("LOW//");
+            currentProcess = removeProcQ(&LO_readyQueue);
+            klog_print("removeProcQLow//");
+            STCK(process_job_start_time);
+            setTIMER(TIMESLICE);
+            klog_print("setTimer//");
+            LDST((void *)&currentProcess->p_s);
+            klog_print("fagiolini3//");
         }
-        
     }
+    else
+    {
+        klog_print("HIGH//");
+        currentProcess = removeProcQ(&HI_readyQueue);
+        klog_print("fagiolini3.2//");
+        LDST(&currentProcess->p_s);
+        klog_print("fagiolini3.1//");
+    }
+
 }
